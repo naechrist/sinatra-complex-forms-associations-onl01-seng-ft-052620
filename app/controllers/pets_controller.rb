211d08316104ea7cb1580c29b4dcd9f409474658
Jenae Christensen <729jenae.christensen@gmail.com>
2020-07-12 21:45:43 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,37 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all 
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    else 
+      @pet.owner = Owner.find_by_id(parmas[:pet][:owner_id])
+    end 
+    @pet.save 
     redirect to "pets/#{@pet.id}"
   end
-
+  
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
-  end
-
-  patch '/pets/:id' do 
-
+  end 
+  
+  post '/pets/:id' do 
     redirect to "pets/#{@pet.id}"
-  end
+  end 
+
+  # get '/pets/:id' do 
+  #   @pet = Pet.find(params[:id])
+  #   erb :'/pets/show'
+  # end
+
+  # patch '/pets/:id' do 
+
+  #   redirect to "pets/#{@pet.id}"
+  # end
 end
